7c211d209ae33b9d667a4441a3e327c4172cae6b
Encke Technologies <james@encke.com>
2020-07-01 19:44:14 -0600

    Update to documents and code to allow custom SSL

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const NK = require( "nk-node" );
+
+let sslForDeploy = ( NK.files.exists( "/var/selfDeploySSLCert" )? NK.files.read( "/var/selfDeploySSLCert" ).trim(): null );
+
+NK.selfDeploy( sslForDeploy, "/var/www/", 0, () => {} );
