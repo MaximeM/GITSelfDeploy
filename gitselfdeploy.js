8a094531999b8f9c661a4b8fe0062935e98a682f
Encke Technologies <james@encke.com>
2020-08-17 09:56:53 -0600

    1.1.0

--- gitselfdeploy.js
+++ gitselfdeploy.js
@@ -1,5 +1,31 @@
-const NK = require( "nk-node" );
+const { execSync }	= require( 'child_process' )
+const express				= require( 'express' )
+const fs						= require( 'fs' )
+const http					= require( 'http' )
+const https					= require( 'https' )
 
-let sslForDeploy = ( NK.files.exists( "/var/selfDeploySSLCert" )? NK.files.read( "/var/selfDeploySSLCert" ).trim(): null );
+const config        = require( './config' )
 
-NK.selfDeploy( sslForDeploy, "/var/www/", 0, () => {} );
+const SSLKey = ( ( config.ssl && fs.existsSync( config.ssl + '/privkey.pem' ) )? fs.readFileSync( ( config.ssl + '/privkey.pem' ), 'utf8' ).trim(): null )
+const SSLCert = ( ( config.ssl && fs.existsSync( config.ssl + '/fullchain.pem' ) )? fs.readFileSync( ( config.ssl + '/fullchain.pem' ), 'utf8' ).trim(): null )
+
+const checkAndRunDeploy = ( req, res ) => {
+  try	{
+    console.log( req.params )
+    //console.log( module.exports.shell( '/bin/sh ' + __dirname + '/gitDeploy.sh ' + deployTo + ' ' + parseInt( pm2ProcessNumber ).toString() ) )
+  }	catch( e )	{
+    console.log( e )
+  }
+  res.end( '' )
+}
+
+const app	= express()
+
+app.post( '/', checkAndRunDeploy )
+app.get( '*', checkAndRunDeploy )
+
+if( SSLKey && SSLCert )	{
+  https.createServer( { key: SSLKey, cert: SSLCert }, app ).listen( config.port, () => console.log( 'Githook enabled with SSL' ) )
+}	else {
+  http.createServer( app ).listen( config.port, () => console.log( 'Githook enabled UNSECURE' ) )
+}
